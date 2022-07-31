66d48b8bdd13d69f024815206eb824d10cfe4521
Claw1200 <carstenwood12@gmail.com>
2022-07-31 23:42:34 +0100

    done

--- spotify/main.py
+++ spotify/main.py
@@ -14,19 +14,19 @@ if __name__ == "__main__":
 
 
     link = extractSpotifyLink(str(input("Enter the link of the playlist: ")))
-    username = str(input("Enter the username of the playlist creator: "))
-
+    username = ""
+    os.chdir(str(input("Enter the path to save the playlist: ")))
     data = pl.call_playlist(username, link)
     print (data)
     for i in range(len(data)):
         row = data.iloc[i].tolist()
         print (row)
-        
         request = str(f"{row[2]} {row[0]} {row[1]} topic")
         #track, artist, album
-
         print (request)
         dh.download_video(request, row[2], row[0], row[1], {row[13]}, {row[6]}, {row[5]}, os.getcwd())
+
+    os.remove(".cache")
 
 
 
